16a33eea4f4a70be6127d2489722ef3eb9752464
iamgruuten <52743433+iamgruuten@users.noreply.github.com>
2021-10-25 14:12:13 +0800

    Init files

---
+++ src/main/java/com/singapore/unlocked/SingaporeUnlockedBackendApplication.java
@@ -0,0 +1,13 @@
+package com.singapore.unlocked;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SingaporeUnlockedBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SingaporeUnlockedBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/singapore/unlocked/SingaporeUnlockedBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.singapore.unlocked;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingaporeUnlockedBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
